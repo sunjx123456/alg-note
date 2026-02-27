842b9abc2994fc3f414e1e92b2872cd47b58e552
sjx <sunjingxiang2006@gmail.com>
2026-02-27 16:22:49 +0800

    b.cpp

---
+++ b.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define N 500010
+#define M 2001
+#define ec ' '
+#define one 1ll
+#define endl '\n'
+const int mod = 998244353;
+#define inf 0x7f7f7f7f
+int n, m;
+
+signed main()
+{
+    ios::sync_with_stdio(0); cin.tie(0), cout.tie(0);
+    cout<<"B"<<endl;
+    return 0;
+}
